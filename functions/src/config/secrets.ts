1e008c29dfcf1154282e6c3f0f0a9b99895463ff
Manoel Júnior <seu-email@gmail.com>
2026-06-28 22:57:51 -0300

    Limit active function secrets to OpenRouter

--- functions/src/config/secrets.ts
+++ functions/src/config/secrets.ts
@@ -1,11 +1,7 @@
 import { defineSecret } from 'firebase-functions/params'
 
 export const openRouterApiKey = defineSecret('OPENROUTER_API_KEY')
-export const azureSpeechKey = defineSecret('AZURE_SPEECH_KEY')
-export const azureSpeechRegion = defineSecret('AZURE_SPEECH_REGION')
 
 export const externalProviderSecrets = [
   openRouterApiKey,
-  azureSpeechKey,
-  azureSpeechRegion,
 ]
